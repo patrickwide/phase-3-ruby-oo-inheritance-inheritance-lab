d1935eb86c909324bfc04876a026a46876009178
patrickwide <patrickwide254@gmail.com>
2023-05-23 20:28:16 +0300

    Complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative 'user'
 
+class Student < User  
+  def initialize(first_name = nil, last_name = nil)
+    super(first_name, last_name)
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
